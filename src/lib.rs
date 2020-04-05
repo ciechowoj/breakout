329b29de2bcc30dabe1c8e88f25bfbb970dde9f4
Wojciech Szęszoł <keepitsimplesirius@gmail.com>
2020-04-05 21:21:52 +0200

    Two black rectangles.

--- src/lib.rs
+++ src/lib.rs
@@ -16,18 +16,42 @@ extern {
     fn alert(s: &str);
 }
 
-
 #[wasm_bindgen]
 pub fn greet() {
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
+
+    let html = document.document_element().expect("document should have a html");
+    let html = html.dyn_into::<web_sys::HtmlElement>()
+        .map_err(|_| ())
+        .unwrap();
+
+    html.style().set_property("height", "100%").unwrap();
+
     let body = document.body().expect("document should have a body");
     document.set_title("Omg! It works!");
+
+    body.style().set_property("margin", "0px").unwrap();
+    body.style().set_property("padding", "0px").unwrap();
+    body.style().set_property("width", "100%").unwrap();
+    body.style().set_property("height", "100%").unwrap();
 
     let canvas = document.create_element("canvas").unwrap();
     let canvas = canvas.dyn_into::<web_sys::HtmlCanvasElement>()
         .map_err(|_| ())
         .unwrap();
+
+    canvas.set_width(640);
+    canvas.set_height(480);
+
+    canvas.style().set_property("border", "none").unwrap();
+    canvas.style().set_property("min-width", "800px").unwrap();
+    canvas.style().set_property("height", "100%").unwrap();
+    canvas.style().set_property("margin-left", "auto").unwrap();
+    canvas.style().set_property("margin-right", "auto").unwrap();
+    canvas.style().set_property("padding-left", "0px").unwrap();
+    canvas.style().set_property("padding-right", "0px").unwrap();
+    canvas.style().set_property("display", "block").unwrap();
 
     body.append_child(&canvas).ok();
 
@@ -45,14 +69,9 @@ pub fn greet() {
     
         let inner : Box<dyn FnMut(JsValue)> = Box::new(move |js_value : JsValue| {
             // context.set_fill_style(&JsValue::from_str("red"));
-            context.clear_rect(0.0, 0.0, 200.0, 200.0);
-
-            context.set_fill_style(&JsValue::from_str("black"));
+            
             if let Some(value) = js_value.as_f64() {
-                context.fill_rect(100.0 * (value * 0.001).fract(), 10.0, 100.0, 100.0);
-            }
-            else {
-                context.fill_rect(100.0, 10.0, 100.0, 100.0);
+                crate::update(&context, value);
             }
 
             (update.value)(Recursive { value: update.value.clone() });
@@ -66,4 +85,19 @@ pub fn greet() {
 
     let update_clone = Rc::new(update);
     update_clone(Recursive { value: update_clone.clone() });
+}
+
+
+pub fn update(context : &CanvasRenderingContext2d, time : f64) {
+
+    let canvas = context.canvas().unwrap();
+
+    let width = canvas.width() as f64;
+    let height = canvas.height() as f64;
+
+    context.clear_rect(0.0, 0.0, width, height);
+    context.set_fill_style(&JsValue::from_str("black"));
+    context.fill_rect((width - 100.0) * (time * 0.0001).fract(), 10.0, 100.0, 100.0);
+
+    context.fill_rect(0.0, 200.0, width, 100.0);
 }
